efd969e4040db2159a955c383278bab868983a9f
j.karippacheril.jacob <j.karippacheril.jacob@fz-juelich.de>
2023-11-24 14:54:25 +0100

    Revised the network_test.py. The changes made are added logging insted of the print statement

--- tests/network_test.py
+++ tests/network_test.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# **********************************************************************************************************************
+# This test script serves to confirm that the Network class maintains its functionality post-modifications.
+# **********************************************************************************************************************
+
 import GasNetSim as gns
 from pathlib import Path
 from numpy.testing import assert_almost_equal
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def network_test():
 
     # Create a network instance with Irish13
     # Initialize the network with nodes and connections from a CSV file in the current directory
-    network = gns.create_network_from_csv(Path('../unit_testing'))
+    network = gns.create_network_from_csv(Path('.'))
 
     # Simulate the network to compute the pressures and flows
     network.simulation(tol=0.0000001)
@@ -23,8 +31,8 @@ def network_test():
 
     # Retrieve the final pressure values from the simulated network nodes
     final_pressure = [node.pressure for node in network.nodes.values()]
-    
-    # Calculate total inflow and outflow over the entire Network    
+
+    # Calculate total inflow and outflow over the entire Network
     # Initialize lists to store node information
     node_indices = []
     inlet_flows = []
@@ -59,12 +67,12 @@ def network_test():
     total_outflow = sum(outlet_flows)
 
     # Check if the final pressure values from the simulation match the expected values
-    assert_almost_equal(final_pressure, expected_final_pressure, 3)
+    assert_almost_equal(final_pressure, expected_final_pressure)
     assert_almost_equal(inlet_flows, outlet_flows)
     assert_almost_equal(total_inflow, total_outflow)
 
     # If the assertion passes, print a message indicating that the test passed
-    print("Test passed: Results match the expected values.")
+    logger.info(f"Test passed: Results match the expected values.")
 
 
 # Check if the script is being run directly
