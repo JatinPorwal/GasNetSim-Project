4f9ddaa91a8cccebc95d17fb263e287b1f171a79
j.karippacheril.jacob <j.karippacheril.jacob@fz-juelich.de>
2023-11-24 18:56:58 +0100

    Revised the validate_pipelines_csv.py to try and reduce the loops and display the respective error with suitable suggestions.  Changes were also made by removing the expected headers and mandatory headers to the main function

--- tests/validate_pipelines_csv.py
+++ tests/validate_pipelines_csv.py
@@ -13,26 +13,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def validate_pipeline_csv_format(file_path):
-    expected_headers = [
-        'pipeline_index',
-        'inlet_index',
-        'outlet_index',
-        'diameter_m',
-        'length_m',
-        ' is_bothDirection',
-        'max_cap_M_m3_per_d',
-        'max_pressure_bar',
-        'remarks'
-    ]
-    
-    mandatory_headers = [
-        'pipeline_index',
-        'inlet_index',
-        'outlet_index',
-        'diameter_m',
-        'length_m'
-    ]
+def validate_pipeline_csv_format(expected_headers, mandatory_headers, file_path):
+
+    # Check whether the file is present or not
     if not (os.path.isfile(file_path)):
         logger.info(f"The file is either missing or not readable")
         return False
@@ -47,11 +30,11 @@ def validate_pipeline_csv_format(file_path):
             return False
         
         # Check each row for missing values in required columns
-        for row in reader:
-            for key in mandatory_headers:
-                if row[key] == '' or row[key] is None or row[key].lower() == 'nan':  # Check for None values or 'nan'
-                    logger.info(f"Missing value in column '{key}' in one or more rows.")
-                    return False
+        for row_num, row in enumerate(reader, start=1):
+            missing_values = [key for key in mandatory_headers if row.get(key, '').strip().lower() in ['', 'nan', None]]
+            if missing_values:
+                logger.info(f"Missing value(s) in column(s) {', '.join(missing_values)} in row {row_num}.")
+                return False
 
     logger.info(f"The CSV file follows the expected format.")
     return True
@@ -59,8 +42,28 @@ def validate_pipeline_csv_format(file_path):
 
 # Check if the script is being run directly
 if __name__ == "__main__":
+    # Defining the expected and mandatory headers for the csv file - pipelines
+    Expected_headers = [
+        'pipeline_index',
+        'inlet_index',
+        'outlet_index',
+        'diameter_m',
+        'length_m',
+        ' is_bothDirection',
+        'max_cap_M_m3_per_d',
+        'max_pressure_bar',
+        'remarks'
+    ]
+
+    Mandatory_headers = [
+        'pipeline_index',
+        'inlet_index',
+        'outlet_index',
+        'diameter_m',
+        'length_m'
+    ]
     # Input the 'File_path' with the path of the pipeline CSV file
     # File_path = '../examples/Irish13/Irish13_pipelines.csv'
     File_path = input("Enter the path of the pipelines.csv.\nFor example "
                       "'../examples/Irish13/Irish13_pipelines.csv':")
-    validate_pipeline_csv_format(File_path)
+    validate_pipeline_csv_format(Expected_headers, Mandatory_headers, File_path)
