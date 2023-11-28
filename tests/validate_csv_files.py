49cd245a4e3a14831ed26d006a5f4713fcf874d4
j.karippacheril.jacob <j.karippacheril.jacob@fz-juelich.de>
2023-11-28 15:49:23 +0100

    In validate_csv_files.py, the class was removed, the list for mandatory and expected headers were made global, now the functions has input argument as filepath

--- tests/validate_csv_files.py
+++ tests/validate_csv_files.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # **********************************************************************************************************************
-# This script ensures the .csv file conforms to the established format guidelines.
+# This script ensures that the .csv file conforms to the established format guidelines.
 # **********************************************************************************************************************
 
 import csv
@@ -12,144 +12,122 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+nodes_expected_headers = ['node_index', 'pressure_pa', 'flow_sm3_per_s', 'flow_MW', 'altitude_m',
+                          'temperature_k', 'gas_composition', 'node_type', 'flow_type', 'longitude',
+                          ' latitude', ' remarks']
 
-class ValidateCsvFiles:
+pipeline_expected_headers = ['pipeline_index', 'inlet_index', 'outlet_index', 'diameter_m', 'length_m',
+                             ' is_bothDirection', 'max_cap_M_m3_per_d', 'max_pressure_bar', 'remarks']
+
+pipeline_mandatory_headers = ['pipeline_index', 'inlet_index', 'outlet_index', 'diameter_m', 'length_m']
+
+
+def validate_pipelines_csv_format(file_path=None):
 
     """
-        CSV File validation class
+            Validates the format of the pipelines CSV file.
+
+            Args:
+            - file_path (str): The path to the CSV file.
+
+            Returns:
+            - bool: True if the file conforms to the expected format, False otherwise.
     """
 
-    def __init__(self, file_path=None, pipeline_expected_headers=None, pipeline_mandatory_headers=None,
-                 nodes_expected_headers=None):
+    # Check whether the file is present or not
+    if not (os.path.isfile(file_path)):
+        logger.info(f"The file pipelines CSV is either missing or not readable")
+        return False
 
-        """
-                Initializes the ValidateCsvFiles class with the path of the CSV file.
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        headers = reader.fieldnames
+        print(headers)
 
-                Parameters:
-                - file_path (str): Path to the CSV file for validation.
-
-                Defines the expected and mandatory headers for pipelines and nodes CSV files.
-        """
-
-        # Defining the expected and mandatory headers for the csv files
-        if nodes_expected_headers is None:
-            self.nodes_expected_headers = ['node_index', 'pressure_pa', 'flow_sm3_per_s', 'flow_MW', 'altitude_m',
-                                           'temperature_k', 'gas_composition', 'node_type', 'flow_type', 'longitude',
-                                           ' latitude', ' remarks']
-        else:
-            self.nodes_expected_headers = nodes_expected_headers
-
-        if pipeline_expected_headers is None:
-            self.pipeline_expected_headers = {'pipeline_index', 'inlet_index', 'outlet_index', 'diameter_m', 'length_m',
-                                              ' is_bothDirection', 'max_cap_M_m3_per_d', 'max_pressure_bar', 'remarks'}
-        else:
-            self.pipeline_expected_headers = pipeline_expected_headers
-
-        if pipeline_mandatory_headers is None:
-            self.pipeline_mandatory_headers = ['pipeline_index', 'inlet_index', 'outlet_index', 'diameter_m', 'length_m']
-        else:
-            self.pipeline_mandatory_headers = pipeline_mandatory_headers
-
-        self.file_path = file_path
-
-    def validate_pipelines_csv_format(self):
-
-        """
-                Validates the format of the pipelines CSV file.
-
-                Returns:
-                - bool: True if the file conforms to the expected format, False otherwise.
-        """
-
-        # Check whether the file is present or not
-        if not (os.path.isfile(self.file_path)):
-            logger.info(f"The file pipelines CSV is either missing or not readable")
+        # Check if headers match the expected format
+        if headers != pipeline_expected_headers:
+            logger.info(f"The pipelines CSV file does not match the expected format.")
             return False
 
-        with open(self.file_path, 'r') as file:
-            reader = csv.DictReader(file, delimiter=';')
-            headers = reader.fieldnames
-
-            # Check if headers match the expected format
-            if headers != self.pipeline_expected_headers:
-                logger.info(f"The pipelines CSV file does not match the expected format.")
+        # Check each row for missing values in required columns
+        for row_num, row in enumerate(reader, start=1):
+            missing_values = [key for key in pipeline_mandatory_headers
+                              if row.get(key, '').strip().lower() in ['', 'nan', None]]
+            if missing_values:
+                logger.info(f"Missing value(s) in column(s) {', '.join(missing_values)} in row {row_num} "
+                            f"in pipelines CSV.")
                 return False
 
-            # Check each row for missing values in required columns
-            for row_num, row in enumerate(reader, start=1):
-                missing_values = [key for key in self.pipeline_mandatory_headers
-                                  if row.get(key, '').strip().lower() in ['', 'nan', None]]
-                if missing_values:
-                    logger.info(f"Missing value(s) in column(s) {', '.join(missing_values)} in row {row_num} "
-                                f"in pipelines CSV.")
-                    return False
+    logger.info(f"The pipelines CSV file follows the expected format.")
+    return True
 
-        logger.info(f"The pipelines CSV file follows the expected format.")
-        return True
 
-    def validate_nodes_csv_format(self):
+def validate_nodes_csv_format(file_path=None):
 
-        """
-                Validates the format of the nodes CSV file.
+    """
+            Validates the format of the nodes CSV file.
 
-                Returns:
-                - bool: True if the file conforms to the expected format, False otherwise.
-        """
+            Args:
+            - file_path (str): The path to the CSV file.
 
-        # Check if the file exists
-        if not os.path.isfile(self.file_path):
-            logger.info(f"The nodes file is either missing or not readable")
+            Returns:
+            - bool: True if the file conforms to the expected format, False otherwise.
+    """
+
+    # Check if the file exists
+    if not os.path.isfile(file_path):
+        logger.info(f"The nodes file is either missing or not readable")
+        return False
+
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file, delimiter=';')
+        headers = reader.fieldnames
+
+        # Check if headers match the expected format
+        if headers != nodes_expected_headers:
+            logger.info(f"The nodes CSV file does not match the expected format.")
             return False
 
-        with open(self.file_path, 'r') as file:
-            reader = csv.DictReader(file, delimiter=';')
-            headers = reader.fieldnames
+        reference_found = False
+        for row_num, row in enumerate(reader, start=1):
 
-            # Check if headers match the expected format
-            if headers != self.nodes_expected_headers:
-                logger.info(f"The nodes CSV file does not match the expected format.")
+            # Check each row to see if at-least one 'reference' value is present or not
+            if row['node_type'].lower() == 'reference':
+                reference_found = True
+
+            # Check each row to see if any 'node_index' value is missing
+            if not row['node_index']:
+                logger.info(f"Missing 'node_index' in row {row_num}.")
                 return False
 
-            reference_found = False
-            for row_num, row in enumerate(reader, start=1):
-
-                # Check each row to see if at-least one 'reference' value is present or not
-                if row['node_type'].lower() == 'reference':
-                    reference_found = True
-
-                # Check each row to see if any 'node_index' value is missing
-                if not row['node_index']:
-                    logger.info(f"Missing 'node_index' in row {row_num}.")
+            # Check if either 'pressure_pa', 'flow_sm3_per_s' or 'flow_MW' value are present in the row
+            if row['node_type'].lower() == 'reference':
+                if not row['pressure_pa']:
+                    logger.info(
+                        f"The 'pressure_pa' value is missing for the 'reference node' located in row {row_num} "
+                        f"of the nodes CSV file.")
                     return False
-
-                # Check if either 'pressure_pa', 'flow_sm3_per_s' or 'flow_MW' value are present in the row
-                if row['node_type'].lower() == 'reference':
-                    if not row['pressure_pa']:
-                        logger.info(
-                            f"The 'pressure_pa' value is missing for the 'reference node' located in row {row_num} "
-                            f"of the nodes CSV file.")
-                        return False
-                elif not row['flow_sm3_per_s']:
-                    if not row['flow_MW']:
-                        logger.info(f"Either the 'flow_sm3_per_s' or 'flow_MW' value is missing "
-                                    f"from row {row_num} of the nodes CSV file. This data is required.")
-                        return False
-                if row['pressure_pa'] and (row['flow_sm3_per_s'] or row['flow_MW']):
-                    logger.info(f"The 'pressure_pa' field cannot be assigned a value when either the "
-                                f"'flow_sm3_per_s' or 'flow_MW' field is provided. This constraint was "
-                                f"violated in row {row_num}.")
+            elif not row['flow_sm3_per_s']:
+                if not row['flow_MW']:
+                    logger.info(f"Either the 'flow_sm3_per_s' or 'flow_MW' value is missing "
+                                f"from row {row_num} of the nodes CSV file. This data is required.")
                     return False
-                if row['flow_sm3_per_s'] and row['flow_MW']:
-                    logger.info(f"The 'flow_sm3_per_s' field cannot be assigned a value together with the "
-                                f"'flow_MW' field. This constraint was violated in row {row_num}.")
-                    return False
-
-            if not reference_found:
-                logger.info(f"No 'reference' value found in the 'node_type' column found in nodes CSV.")
+            if row['pressure_pa'] and (row['flow_sm3_per_s'] or row['flow_MW']):
+                logger.info(f"The 'pressure_pa' field cannot be assigned a value when either the "
+                            f"'flow_sm3_per_s' or 'flow_MW' field is provided. This constraint was "
+                            f"violated in row {row_num}.")
+                return False
+            if row['flow_sm3_per_s'] and row['flow_MW']:
+                logger.info(f"The 'flow_sm3_per_s' field cannot be assigned a value together with the "
+                            f"'flow_MW' field. This constraint was violated in row {row_num}.")
                 return False
 
-        logger.info(f"The nodes CSV file follows the expected format.")
-        return True
+        if not reference_found:
+            logger.info(f"No 'reference' value found in the 'node_type' column found in nodes CSV.")
+            return False
+
+    logger.info(f"The nodes CSV file follows the expected format.")
+    return True
 
 
 # Check if the script is being run directly
@@ -157,6 +135,6 @@ if __name__ == "__main__":
     # Input the 'File_path' with the path of the pipeline CSV file
     File_path = input("Enter the path of the .csv file.\nFor example "
                       "'../examples/Irish13/Irish13_pipelines.csv':")
-    val = ValidateCsvFiles(file_path=File_path)
-    val.validate_pipelines_csv_format()
-    val.validate_nodes_csv_format()
+
+    validate_pipelines_csv_format(file_path=File_path)
+    validate_nodes_csv_format(file_path=File_path)
