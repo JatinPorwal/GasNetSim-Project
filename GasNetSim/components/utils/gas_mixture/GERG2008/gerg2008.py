6e70962c27ce86e3a569ab7a4b354ccbd31cbb00
Yifei Lu <yi.lu@fz-juelich.de>
2024-02-06 10:56:13 +0100

    Fix error in heating value calculation

--- GasNetSim/components/utils/gas_mixture/GERG2008/gerg2008.py
+++ GasNetSim/components/utils/gas_mixture/GERG2008/gerg2008.py
@@ -182,7 +182,7 @@ class GasMixtureGERG2008:
         self.PropertiesGERG()
 
         self.HHV_J_per_m3 = self.CalculateHeatingValue(comp=composition, hhv=True, parameter="volume")
-        self.HHV_J_per_sm3 = self.HHV_J_per_m3 * self.P * 1000 / atm * 288.15 / self.T
+        self.HHV_J_per_sm3 = self.HHV_J_per_m3 / self.P / 1000 * atm / 288.15 * self.T
         self.HHV_J_per_kg = self.CalculateHeatingValue(comp=composition, hhv=True, parameter="mass")
 
     def CalculateHeatingValue(self, comp, hhv, parameter):
@@ -332,9 +332,9 @@ class GasMixtureGERG2008:
         else:
             # returns heating value in J/m3
             if hhv:
-                heating_value = HHV / self.MolarDensity * 1e3
+                heating_value = HHV * self.MolarDensity * 1e3
             else:
-                heating_value = LHV / self.MolarDensity * 1e3
+                heating_value = LHV * self.MolarDensity * 1e3
 
         return heating_value
 
